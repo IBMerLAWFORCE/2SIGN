62a78e9611945772adeca29487d43721fea89cb2
Ted <ted@renocompass.ca>
2018-02-24 20:53:58 -0500

    combine update
    combine update

---
+++ src/pages/finalform/finalform.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { FinalformPage } from './finalform';
+
+@NgModule({
+  declarations: [
+    FinalformPage,
+  ],
+  imports: [
+    IonicPageModule.forChild(FinalformPage),
+  ],
+})
+export class FinalformPageModule {}
